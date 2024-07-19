698039aeed31a95acfb6bfa1bd33b192c2f2087a
Willy Yeremi <willyyeremi@gmail.com>
2024-07-19 13:26:53 +0700

    build:

--- run.bat
+++ run.bat
@@ -1,9 +1,8 @@
 @echo off
 
+mkdir ".\database\postgresql"
 docker-compose up -d
 python -m pip install virtualenv
 python -m virtualenv ./api/.venv
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
-.\api\.venv\Scripts\activate
-python -m pip install -r ./api/requirements.txt
-python ./api/app.py
+.\api\.venv\Scripts\python.exe -m pip install -r ./api/requirements.txt
+.\api\.venv\Scripts\python.exe ./api/app.py
